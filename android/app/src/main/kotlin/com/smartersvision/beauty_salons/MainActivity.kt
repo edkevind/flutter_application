ea89f92bd7d260d0f1a7e932c30810157413ef4d
= <=>
2022-11-29 19:54:23 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/smartersvision/beauty_salons/MainActivity.kt
@@ -0,0 +1,13 @@
+/*
+ * File name: MainActivity.kt
+ * Last modified: 2022.02.13 at 11:18:47
+ * Author: Doyatama
+ * Copyright (c) 2022
+ */
+
+package com.smartersvision.beauty_salons
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
